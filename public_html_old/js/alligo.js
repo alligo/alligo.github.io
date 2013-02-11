c4d5460f3a81214659fc6268ad456b7c03df51c4
Emerson Rocha Luiz <emerson@webdesign.eng.br>
2013-02-11 01:31:05 -0200

    Implementado CSS com SASS

---
+++ public_html_old/js/alligo.js
@@ -0,0 +1,5 @@
+document.getElementById('email-alligo').innerHTML = "alligo" + "@" + "alligo.com.br";
+document.getElementById('email-emerson').innerHTML = "emerson" + "@" + "alligo.com.br";
+document.getElementById('email-juliana').innerHTML = "juliana" + "@" + "alligo.com.br";
+
+
