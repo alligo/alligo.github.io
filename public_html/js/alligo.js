329943b9d99aed35196fab672e02664a1922018d
Emerson Rocha Luiz <emerson@webdesign.eng.br>
2013-02-12 03:21:07 -0200

    Ultimos detalhes antes da primeira publicacao online

--- public_html/js/alligo.js
+++ public_html/js/alligo.js
@@ -1,3 +1,4 @@
+//<script src="./js/alligo.js"></script>
 document.getElementById('email-alligo').innerHTML = "alligo" + "@" + "alligo.com.br";
 document.getElementById('email-emerson').innerHTML = "emerson" + "@" + "alligo.com.br";
 document.getElementById('email-juliana').innerHTML = "juliana" + "@" + "alligo.com.br";
